99b2867179eed7f0a118adb78f49c176f46a1d96
Dóra Erdélyi <dora.erdelyi155@gmail.com>
2025-11-28 12:15:27 +0100

    create project module structure

---
+++ microservices/auth-service/src/test/java/com/wandersmart/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wandersmart.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ microservices/traveller-service/src/test/java/com/wandersmart/travellerservice/TravellerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wandersmart.travellerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravellerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
