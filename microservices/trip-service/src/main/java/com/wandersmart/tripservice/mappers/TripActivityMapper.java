06882073dc5c160865f4f44d5df12648aea46e17
Dóra Erdélyi <dora.erdelyi155@gmail.com>
2026-01-06 22:39:07 +0100

    implement method to convert DTO to Trip Activity using MapStruct

--- microservices/trip-service/src/main/java/com/wandersmart/tripservice/mappers/TripActivityMapper.java
+++ microservices/trip-service/src/main/java/com/wandersmart/tripservice/mappers/TripActivityMapper.java
@@ -1,7 +1,9 @@
 package com.wandersmart.tripservice.mappers;
 
 
+import com.wandersmart.tripservice.dto.TripActivityRequestDTO;
 import com.wandersmart.tripservice.dto.TripActivityResponseDTO;
+import com.wandersmart.tripservice.model.Trip;
 import com.wandersmart.tripservice.model.TripActivity;
 import org.mapstruct.Mapper;
 
@@ -9,5 +11,5 @@ import org.mapstruct.Mapper;
 public interface TripActivityMapper {
 
     TripActivityResponseDTO toDTO(TripActivity tripActivity);
-
+    TripActivity toTripActivity(TripActivityRequestDTO tripActivityRequestDTO, Trip trip);
 }
