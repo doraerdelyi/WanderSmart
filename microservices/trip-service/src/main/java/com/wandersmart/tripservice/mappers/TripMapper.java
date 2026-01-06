b356846af44b081097e021b80965d8729985b82f
Dóra Erdélyi <dora.erdelyi155@gmail.com>
2026-01-06 17:12:05 +0100

    add MapStruct as dependency and configure annotation processing

--- microservices/trip-service/src/main/java/com/wandersmart/tripservice/mappers/TripMapper.java
+++ microservices/trip-service/src/main/java/com/wandersmart/tripservice/mappers/TripMapper.java
@@ -11,12 +11,12 @@ import java.util.stream.Collectors;
 
 public class TripMapper {
 
-    public static TripResponseDTO tripToTripResponseDTO(Trip trip) {
+    public static TripResponseDTO toDTO(Trip trip) {
         return new TripResponseDTO(trip.getTripId(), trip.getName(), trip.getStartDate(), trip.getEndDate());
     }
-    public static TripDetailsResponseDTO tripToTripDetailsResponseDTO(Trip trip) {
+    public static TripDetailsResponseDTO toDTOWithDetails(Trip trip) {
         //List<TripActivity> tripActivities = trip.getTripActivities();
-        List<TripActivityResponseDTO> tripActivityDTOS = tripActivities.stream().map(this::convertTripActivityToTripActivityDTO).collect(Collectors.toList());
+        List<TripActivityResponseDTO> tripActivityDTOS = tripActivities.stream().map(TripActivityMapper::toDTO).collect(Collectors.toList());
         return new TripDetailsResponseDTO(trip.getTripId(), trip.getName(), trip.getStartDate(), trip.getEndDate(), tripActivityDTOS);
     }
 
