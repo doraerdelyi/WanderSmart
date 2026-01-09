4f064de13c90c7d678e1d28e79259330dd8653fd
Dóra Erdélyi <dora.erdelyi155@gmail.com>
2026-01-09 12:37:39 +0100

    add detailed place data to trip activity dto

--- microservices/trip-service/src/main/java/com/wandersmart/tripservice/dto/TripActivityResponseDTO.java
+++ microservices/trip-service/src/main/java/com/wandersmart/tripservice/dto/TripActivityResponseDTO.java
@@ -1,6 +1,7 @@
 package com.wandersmart.tripservice.dto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-public record TripActivityResponseDTO(String placeId, LocalDateTime visitTime) {
+public record TripActivityResponseDTO(String placeId, String name, double rating, int priceLevel, List<String> openingHours, List<String> photos, double latitude, double longitude, LocalDateTime visitTime) {
 }
