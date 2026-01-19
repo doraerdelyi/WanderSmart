f15e9215b72c8d27cce206e7db3ab30b8ac437ed
Dóra Erdélyi <dora.erdelyi155@gmail.com>
2026-01-19 17:55:55 +0100

    implement Grpc Client in trip service

--- microservices/trip-service/src/main/java/com/wandersmart/tripservice/grpc/TripServiceGrpcClient.java
+++ microservices/trip-service/src/main/java/com/wandersmart/tripservice/grpc/TripServiceGrpcClient.java
@@ -1,17 +1,44 @@
 package com.wandersmart.tripservice.grpc;
 
+import com.wandersmart.grpc.place.PlaceRequest;
+import com.wandersmart.grpc.place.PlaceResponse;
 import com.wandersmart.grpc.place.PlaceServiceGrpc;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 @Service
 public class TripServiceGrpcClient {
 
-    @GrpcClient("place-service")
+    private final PlaceGrpcMapper mapper;
     private final PlaceServiceGrpc.PlaceServiceBlockingStub blockingStub;
 
-
-    public TripServiceGrpcClient(PlaceServiceGrpc.PlaceServiceBlockingStub blockingStub) {
+    public TripServiceGrpcClient(@GrpcClient("place-service") PlaceServiceGrpc.PlaceServiceBlockingStub blockingStub, PlaceGrpcMapper mapper) {
+        this.mapper = mapper;
         this.blockingStub = blockingStub;
+    }
+
+  public Map<String, PlaceDTO> getPlaceById(List<String> placeIds) {
+      if (placeIds == null || placeIds.isEmpty()) {
+          return Collections.emptyMap();
+      }
+
+      PlaceRequest request = PlaceRequest.newBuilder().addAllPlaceId(placeIds).build();
+
+      PlaceResponse response = blockingStub.getPlaceById(request);
+
+      return mapper.toDTOList(response.getPlacesList())
+              .stream()
+              .collect(Collectors.toMap(
+                      com.wandersmart.grpc.place.Place::getPlaceId,
+                      Function.identity()
+              ));
+
     }
 }
