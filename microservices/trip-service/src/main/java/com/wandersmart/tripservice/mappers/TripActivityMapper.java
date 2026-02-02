5abbec7d496a104141aac63a3725d6c3ea1405cc
Dóra Erdélyi <dora.erdelyi155@gmail.com>
2026-02-02 13:29:22 +0100

    add explicit mapping when converting Trip Activity to DTO

--- microservices/trip-service/src/main/java/com/wandersmart/tripservice/mappers/TripActivityMapper.java
+++ microservices/trip-service/src/main/java/com/wandersmart/tripservice/mappers/TripActivityMapper.java
@@ -1,15 +1,27 @@
 package com.wandersmart.tripservice.mappers;
 
 
+import com.wandersmart.tripservice.dto.PlaceDTO;
 import com.wandersmart.tripservice.dto.TripActivityRequestDTO;
 import com.wandersmart.tripservice.dto.TripActivityResponseDTO;
 import com.wandersmart.tripservice.model.Trip;
 import com.wandersmart.tripservice.model.TripActivity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TripActivityMapper {
 
-    TripActivityResponseDTO toDTO(TripActivity tripActivity);
+
+    @Mapping(target = "placeId", source = "place.placeId")
+    @Mapping(target = "name", source = "place.name")
+    @Mapping(target = "rating", source = "place.rating")
+    @Mapping(target = "priceLevel", source = "place.priceLevel")
+    @Mapping(target = "openingHours", source = "place.openingHours")
+    @Mapping(target = "photos", source = "place.photos")
+    @Mapping(target = "latitude", source = "place.latitude")
+    @Mapping(target = "longitude", source = "place.longitude")
+    @Mapping(target = "visitTime", source = "tripActivity.visitTime")
+    TripActivityResponseDTO toDTO(TripActivity tripActivity, PlaceDTO place);
     TripActivity toTripActivity(TripActivityRequestDTO tripActivityRequestDTO, Trip trip);
 }
